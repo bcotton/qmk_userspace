b72e9b61270673f22759f2cf5ca3a1a9b48414c4
Bob Cotton <bob.cotton@grafana.com>
2024-02-07 21:28:07 -0700

    Porting Iris to userspace

---
+++ keyboards/keebio/iris/keymaps/bcotton/rules.mk
@@ -0,0 +1,5 @@
+TAP_DANCE_ENABLE = yes
+# BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+# RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+CONSOLE_ENABLE = no        # Console for debug
+DYNAMIC_TAPPING_TERM_ENABLE = yes
